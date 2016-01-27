fddc9d01880172328199f8f116df2446639262b3
albadash <mattjalba@gmail.com>
2016-01-26 19:57:23 -0700

    Added More Advanced Test Cases
    I cuoldn't figure out how to sort tweets in chronological order.

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -2,6 +2,11 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by malba on 1/26/16.
  */
@@ -14,15 +19,55 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     public void testAddTweet() {
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("hello");
-        tweets.add(tweet);
+        tweets.addTweet(tweet);
         assertTrue(tweets.hasTweet(tweet));
+        try {
+            tweets.addTweet(tweet);
+            Assert.fail("Should have thrown InvalidArgumentException.");
+        } catch (IllegalArgumentException e) {
+            // everything is peaches and cream!
+        }
+    }
+
+    public void testGetTweets() {
+        TweetList tweets = new TweetList();
+        ArrayList<Tweet> returnedTweets = tweets.getTweets();
+        assertTrue(returnedTweets.isEmpty());
+
+        Tweet tweet;
+        int threshold = 50;
+
+        // add a bunch of tweets
+        for (int i = 0; i < threshold; i++) {
+            tweet = new NormalTweet("hello" + i);
+            tweets.addTweet(tweet);
+        }
+
+        // get tweets and make sure everything is there and in order
+        returnedTweets = tweets.getTweets();
+        for (int j = 0; j < threshold; j++) {
+            assertTrue(returnedTweets.contains(tweets.getTweet(j)));
+            if (j != threshold - 1) {
+                assertTrue(returnedTweets.get(j).getDate().compareTo(returnedTweets.get(j + 1).getDate()) <= 0);
+            }
+        }
+
+        // clear all that and try testing dates not in order
+        tweets = new TweetList();
+        Date earlier_date = new Date();
+        tweet = new NormalTweet(new Date(), "hello");
+        tweets.addTweet(tweet);
+        tweet = new NormalTweet(earlier_date, "hello");
+        tweets.addTweet(tweet);
+        returnedTweets = tweets.getTweets();
+        assertTrue(returnedTweets.get(0).getDate().compareTo(returnedTweets.get(1).getDate()) <= 0);
     }
 
     public void testHasTweet() {
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("hello");
         assertFalse(tweets.hasTweet(tweet));
-        tweets.add(tweet);
+        tweets.addTweet(tweet);
         tweets.hasTweet(tweet);
         assertTrue(tweets.hasTweet(tweet));
     }
@@ -30,18 +75,30 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     public void testGetTweet() {
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("hello");
-        tweets.add(tweet);
+        tweets.addTweet(tweet);
         Tweet returnedTweet = tweets.getTweet(0);
         assertEquals(tweet.getMessage(), returnedTweet.getMessage());
         assertEquals(tweet.getDate(), returnedTweet.getDate());
     }
 
-    public void testDeleteTweet() {
+    public void testRemoveTweet() {
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("hello");
-        tweets.add(tweet);
+        tweets.addTweet(tweet);
         assertTrue(tweets.hasTweet(tweet));
-        tweets.delete(tweet);
+        tweets.removeTweet(tweet);
         assertFalse(tweets.hasTweet(tweet));
+    }
+
+    public void testGetCount() {
+        TweetList tweets = new TweetList();
+        Tweet tweet;
+        int threshold = 50;
+
+        for (int i = 0; i < threshold; i++) {
+            assertEquals(tweets.getCount(), i);
+            tweet = new NormalTweet("test"+i);
+            tweets.addTweet(tweet);
+        }
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,6 +1,7 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by malba on 1/26/16.
@@ -9,11 +10,26 @@ public class TweetList {
 
     private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 
-    public void add(Tweet tweet) {
-        tweets.add(tweet);
+    public void addTweet(Tweet tweet) {
+        if (this.hasTweet(tweet)) {
+            throw new IllegalArgumentException();
+        } else {
+            tweets.add(tweet);
+        }
     }
 
-    public boolean hasTweet (Tweet tweet) {
+    public ArrayList<Tweet> getTweets() {
+
+        TweetList sortedTweets = new TweetList();
+        sortedTweets.tweets.addAll(tweets);
+
+        // here's where I should be sorting this damn list
+        //Collection.sort(sortedTweets.tweets);
+
+        return sortedTweets.tweets;
+    }
+
+    public boolean hasTweet(Tweet tweet) {
         return tweets.contains(tweet);
     }
 
@@ -21,7 +37,11 @@ public class TweetList {
         return tweets.get(index);
     }
 
-    public void delete(Tweet tweet) {
+    public void removeTweet(Tweet tweet) {
         tweets.remove(tweet);
+    }
+
+    public int getCount() {
+        return tweets.size();
     }
 }
